3683ae7dc77b1cecfcb0f9739ee2de9d55806cb1
mike <mike10670229@gmail.com>
2025-08-28 23:40:21 +0800

    MIKE

---
+++ test/src/main/java/net/mcreator/test/item/TESTItem.java
@@ -0,0 +1,9 @@
+package net.mcreator.test.item;
+
+import net.minecraft.world.item.Item;
+
+public class TESTItem extends Item {
+	public TESTItem(Item.Properties properties) {
+		super(properties);
+	}
+}
